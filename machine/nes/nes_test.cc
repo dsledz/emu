8563cac8c21c46c2fd8dd9c3ed178da7047aacf6
Dan Sledz <dsledz@gmail.com>
2016-05-03 22:36:04 -0700

    NES test execution

---
+++ machine/nes/nes_test.cc
@@ -0,0 +1,48 @@
+/*
+ * Copyright (c) 2016, Dan Sledz
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * Redistributions of source code must retain the above copyright notice, this
+ * list of conditions and the following disclaimer.
+ * Redistributions in binary form must reproduce the above copyright notice,
+ * this list of conditions and the following disclaimer in the documentation
+ * and/or other materials provided with the distribution.
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ */
+
+#include "gtest/gtest.h"
+#include "emu/emu.h"
+
+#include "machine/nes/nes.h"
+
+using namespace EMU;
+using namespace NESMachine;
+
+TEST(NesTest, mario3)
+{
+    NES machine;
+
+    machine.load_rom("mario3.nes");
+    FrameBuffer fb;
+
+    Core::log.set_level(LogLevel::Trace);
+    machine.reset();
+    machine.set_frame_buffer(&fb);
+    machine.poweron();
+
+    while (true)
+        machine.run();
+}
