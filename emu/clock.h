656c2377f629ff2b1711e7d1d1ed1adff5dc3008
Dan Sledz <dsledz@gmail.com>
2016-09-11 03:11:55 -0700

    EMU: Crystal changes

--- emu/clock.h
+++ emu/clock.h
@@ -19,6 +19,11 @@ public:
     void add_device_clock(EmuClockBase *clock);
     void remove_device_clock(EmuClockBase *clock);
 
+    const EmuTime now(void) const;
+    const EmuTime current(void) const;
+    EmuClockBase *oldest(void);
+    EmuClockBase *newest(void);
+
     virtual void line(Line line, LineState state);
     virtual void reset(void);
     virtual void execute(void);
@@ -26,8 +31,17 @@ public:
 
 private:
 
+    void update_stats(void);
+    void publish(void);
+
     std::vector<EmuClockBase *> m_clocks;
     std::unique_ptr<Task> m_task;
+
+    EmuTime m_now;        /**< Wall time */
+    EmuTime m_current;    /**< Simulation target time */
+
+    EmuTime m_newest;
+    EmuTime m_oldest;
 };
 
 };
